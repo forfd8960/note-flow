006e271563871fe2627b0bb431b0e5c093aa5217
forfd8960 <forfd8960@gmail.com>
2026-09-08 09:43:14 +0800

    Step 02 - Enter and Leave Alter Screen

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,48 @@
 use std::io::{Write, stdout};
 
 use crossterm::{
+    cursor::{Hide, MoveTo, Show},
     event::{self, Event, KeyCode},
-    terminal::{disable_raw_mode, enable_raw_mode},
+    execute,
+    terminal::{
+        Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
+        enable_raw_mode,
+    },
 };
 
 fn main() -> anyhow::Result<()> {
     enable_raw_mode()?;
+    let mut out = stdout();
+    execute!(stdout(), EnterAlternateScreen, Hide, Clear(ClearType::All))?;
 
-    print!("we are in raw mode");
-    println!();
-    print!("Note Flow - A Simple Text Editor");
-    println!();
+    // move to col, row
+    execute!(&out, MoveTo(0, 0))?;
+    print!("(0, 0), Top Left");
+    execute!(&out, MoveTo(0, 5))?;
+    print!("Row 5");
+    execute!(&out, MoveTo(39, 0))?;
+    print!("Column 39");
+    out.flush()?;
 
     loop {
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q') => break,
                 KeyCode::Char(c) => {
-                    print!("{}", c);
+                    let _ = write_char(&mut out, c)?;
                 }
-                KeyCode::Enter => {
-                    println!();
-                }
-                other => print!("{:?}", other),
+                _ => {}
             }
         }
     }
 
+    execute!(&out, LeaveAlternateScreen, Show)?;
     disable_raw_mode()?;
+    Ok(())
+}
+
+fn write_char<W: Write>(w: &mut W, c: char) -> anyhow::Result<()> {
+    write!(w, "{:?}", c)?;
+    w.flush()?;
     Ok(())
 }
