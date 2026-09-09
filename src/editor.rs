ae83ee93f10153076218c06290088320e0dba0fb
forfd8960 <forfd8960@gmail.com>
2026-09-09 14:42:58 +0800

    Step 07 - Insert char, delete char, insert new line

--- src/editor.rs
+++ src/editor.rs
@@ -26,11 +26,48 @@ impl Document {
         self.lines.len()
     }
 
-    pub fn line(&self, idx: usize) -> String {
-        let line = self.lines.get(idx);
-        match line {
-            Some(l) => l.clone(),
-            None => format!(""),
+    pub fn line(&self, idx: usize) -> &str {
+        self.lines.get(idx).map(String::as_str).unwrap_or("")
+    }
+
+    /// insert char: c at pos for line
+    pub fn insert_char(&mut self, line: usize, pos: usize, c: char) -> (usize, usize) {
+        if line >= self.lines.len() {
+            self.lines.insert(line + 1, String::new());
+        }
+
+        let insert_pos = pos.min(self.lines[line].len());
+        self.lines[line].insert(insert_pos, c);
+
+        (line, insert_pos + c.len_utf8())
+    }
+
+    /// insert new line at: post in line
+    pub fn insert_new_line(&mut self, line: usize, pos: usize) -> (usize, usize) {
+        if line >= self.lines.len() {
+            self.lines.push(String::new());
+            return (line, 0);
+        }
+
+        let ip = pos.min(self.lines[line].len());
+        let new_line = self.lines[line].split_off(ip);
+        self.lines.insert(line + 1, new_line);
+        (line + 1, 0)
+    }
+
+    /// delete char before idx: pos
+    pub fn delete_char(&mut self, line: usize, pos: usize) -> (usize, usize) {
+        if pos > 0 {
+            let dp = pos.min(self.lines[line].len());
+            self.lines[line].remove(dp - 1);
+            (line, dp - 1)
+        } else if line > 0 {
+            // delete char at the begin of line, put cursor_x at end of prev line
+            let l = self.lines.remove(line);
+            self.lines[line - 1].push_str(&l);
+            (line - 1, self.lines[line - 1].len())
+        } else {
+            (line, 0)
         }
     }
 }
@@ -103,6 +140,30 @@ impl Editor {
         self.cursor_x = line_count as u16;
     }
 
+    fn insert_char(&mut self, c: char) {
+        let (row, col) = self
+            .doc
+            .insert_char(self.cursor_y as usize, self.cursor_x as usize, c);
+        self.cursor_y = row as u16;
+        self.cursor_x = col as u16;
+    }
+
+    fn delete_char(&mut self) {
+        let (row, col) = self
+            .doc
+            .delete_char(self.cursor_y as usize, self.cursor_x as usize);
+        self.cursor_y = row as u16;
+        self.cursor_x = col as u16;
+    }
+
+    fn insert_new_line(&mut self) {
+        let (row, col) = self
+            .doc
+            .insert_new_line(self.cursor_y as usize, self.cursor_x as usize);
+        self.cursor_y = row as u16;
+        self.cursor_x = col as u16;
+    }
+
     /// scroll adjust the row offset
     /// when scroll up, cursor_y may small than row_offset, so adjust row_offset
     /// to be cursor_y value
@@ -137,6 +198,9 @@ impl Editor {
             Key::PageDown => self.move_page_down(doc_len),
             Key::Home => self.move_home(),
             Key::End => self.move_end(line.len()),
+            Key::Char(c) => self.insert_char(c),
+            Key::Backspace => self.delete_char(),
+            Key::Enter => self.insert_new_line(),
             Key::Ctrl('q') | Key::Ctrl('Q') => return Ok(true),
             _ => {}
         }
